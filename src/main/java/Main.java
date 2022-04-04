730719360eaae77d02be3c583d2397f34f126ff7
martynovov@yahoo.com <martynovov@yahoo.com>
2022-04-04 14:50:36 +0300

    csvtojson final commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,55 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.*;
+import com.opencsv.bean.*;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json, "data.json");
+    }
+
+    public static List<Employee> parseCSV (String[] columnMapping, String fileName) {
+        List<Employee> staff = null;
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy =
+                new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            staff = csv.parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return staff;
+    }
+
+    public static String listToJson (List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>(){}.getType();
+        String json = gson.toJson(list, listType);
+        return json;
+    }
+
+    public static void writeString (String json, String fileName) {
+        try(Writer writer = new FileWriter(fileName)) {
+            writer.write(json);
+        } catch (IOException e) {
+
+        }
+    }
+}
