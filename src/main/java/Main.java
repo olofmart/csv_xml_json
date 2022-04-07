0fb47503727178757995f8f46d55ca518c3d1d0f
martynovov@yahoo.com <martynovov@yahoo.com>
2022-04-07 12:39:27 +0300

    final jsonparcer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.opencsv.*;
 import com.opencsv.bean.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -13,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
@@ -27,6 +31,33 @@ public class Main {
         }
         String json1 = listToJson(list1);
         writeString(json1, "data2.json");
+        String json2 = readString("new_data.json");
+        List<Employee> list2 = jsonToList(json);
+        list2.stream().forEach(System.out::println);
+    }
+
+    public static List<Employee> jsonToList (String json) throws ParseException{
+        List<Employee> list = new ArrayList<>();
+        Employee employee = new Employee();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        Type listType = new TypeToken<List<Employee>>(){}.getType();
+        list = gson.fromJson(json, listType);
+
+        return list;
+    }
+
+    public static String readString (String fileName) {
+        StringBuilder str = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                str.append(reader.readLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return str.toString();
     }
 
     public static List<Employee> parseCSV (String[] columnMapping, String fileName) {
@@ -48,7 +79,7 @@ public class Main {
 
     public static String listToJson (List<Employee> list) {
         GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
+        Gson gson = builder.setPrettyPrinting().create();
         Type listType = new TypeToken<List<Employee>>(){}.getType();
         String json = gson.toJson(list, listType);
         return json;
@@ -77,6 +108,7 @@ public class Main {
                 employee.id = Long.parseLong(getValue(element, "id"));
                 employee.firstName = getValue(element, "firstName");
                 employee.lastName = getValue(element, "lastName");
+                employee.country = getValue(element, "country");
                 employee.age = Integer.parseInt(getValue(element, "age"));
                 staff.add(employee);
                 }
